2b7633eecad0cc36cd8cfceb78c9ad7758604b92
RLogik <r.bitlogik@gmail.com>
2022-03-02 23:47:04 +0100

    master > master: src - deep key change

--- src/core/utils.js
+++ src/core/utils.js
@@ -10,9 +10,12 @@
 
 const yaml_key_to_js_key = (key) => (key.replace(/-/g, '_'));
 
-const yaml_to_js_dictionary = (data) => {
-    let entries = Object.entries(data);
-    entries = entries.map(([key, value]) => ({[yaml_key_to_js_key(key)]: value}));
+const yaml_to_js_dictionary = (data, deep = false) => {
+    if (!( data.constructor == Object )) return data;
+    const revalue = deep ? (value) => (value) : (value) => yaml_to_js_dictionary(value, true);
+    const entries = Object.entries(data)
+        .map(([key, value]) => ([yaml_key_to_js_key(key), revalue(value)]))
+        .map(([key, value]) => ({ [key]: value }));
     return Object.assign({}, ...entries);
 }
 
