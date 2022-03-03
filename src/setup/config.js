ca4b08cd5bf40286f3028c69d0c6e92c4d337517
RLogik <r.bitlogik@gmail.com>
2022-03-03 12:19:29 +0100

    dev_suffix > master: src - refactored recognition and filter methods

--- src/setup/config.js
+++ src/setup/config.js
@@ -20,8 +20,7 @@ const COMMANDS = (CONFIG['commands'] || [])
     .map((options) => {
         const { aspects } = options;
         const { match } = aspects;
-        aspects['match'] = new RegExp(match);
-        options['command'] = aspects;
+        aspects.match = new RegExp(match);
         return options;
     });
 const DEFAULT_LANGUAGE = CONFIG['default-language'] || 'en';
@@ -31,30 +30,8 @@ const TRANSLATIONS = new TranslatedTexts(LANGUAGE || {}, DEFAULT_LANGUAGE);
 /****************************************************************
  * METHODS
  ****************************************************************/
-const COMMAND_PATTERN = /^(.*?)\@(.*)$/;
-const strip_botname = (text, botname) => {
-    if (COMMAND_PATTERN.test(text)) {
-        const _text = text.replace(COMMAND_PATTERN, `$1`).trim();
-        const _botname = text.replace(COMMAND_PATTERN, `$2`).trim();
-        return { text: _text, botname: _botname, verified: botname == _botname };
-    }
-    const _text = text.trim();
-    return { text: _text, botname: '', verified: true };
-}
 
 const get_translation = (lang, text) => (TRANSLATIONS.value(lang, text));
-const get_command_by_condition = (condition) => COMMANDS.filter(condition);
-const get_command_by_command = (text_, botname_) => {
-    // extract information about bot addressed:
-    const { text, botname, verified } = strip_botname(text_, botname_);
-    if (!verified) return [];
-    // condition: bot names must match if command is strict, and command must match
-    const condition = ({ aspects }) => {
-        const { match, strict } = aspects;
-        return ((!strict || botname == botname_) && match.test(text));
-    };
-    return get_command_by_condition(condition);
-};
 
 /****************************************************************
  * EXPORTS
@@ -65,6 +42,5 @@ module.exports = {
     COMMANDS,
     DEFAULT_LANGUAGE,
     SUPPORTED_LANGUAGES,
-    get_command_by_command,
     get_translation
 };
