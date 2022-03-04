dd012f8c8b8d691f6ab9e96c24f032c7845036ef
RLogik <r.bitlogik@gmail.com>
2022-03-04 16:21:12 +0100

    staging > master: cleaner passing of parameters

--- src/parts/actions.js
+++ src/parts/actions.js
@@ -27,7 +27,7 @@ const {
  * METHODS universal action
  ****************************************************************/
 
-const universal_action = async (bot, ctx, command_options, arguments, { debug, delete_calls }) => {
+const universal_action = async (bot, ctx, command_options, arguments, options) => {
     const user = await get_user_from_context(bot, ctx);
     const msg = ctx.update.message;
     const { reply_to_message } = msg;
@@ -46,23 +46,24 @@ const universal_action = async (bot, ctx, command_options, arguments, { debug, d
     if (!user_has_rights(user, rights)) return;
 
     if ('redirect' in aspects) {
-        return action_on_redirect(bot, arguments, msg, reply_to_message, aspects, text, { debug, delete_calls });
+        return action_on_redirect(bot, arguments, msg, reply_to_message, aspects, text, options);
     }
 
     switch (command) {
         case command.match(/^\/pin(?:|_(.*))$/)?.input:
             const [ flag ] = arguments;
             if (flag === 'all') {
-                return action_on_pin_all_languages(bot, msg, text, { debug, delete_calls });
+                return action_on_pin_all_languages(bot, msg, text, options);
             }
-            return action_on_pin_one_language(bot, arguments, msg, text, { debug, delete_calls });
+            return action_on_pin_one_language(bot, arguments, msg, text, options);
         case '/hello':
+            const { debug } = options;
             if (debug) {
                 const user = await get_user_from_context(bot, ctx);
-                return action_on_hello(bot, user, arguments, msg, reply_to_message, text, { debug, delete_calls });
+                return action_on_hello(bot, user, arguments, msg, reply_to_message, text, options);
             }
         case '/help':
-            return action_on_help(bot, arguments, msg, reply_to_message, text, { debug, delete_calls });
+            return action_on_help(bot, arguments, msg, reply_to_message, text, options);
         default:
             return action_delete_and_ignore();
     }
@@ -80,7 +81,7 @@ const action_delete_and_ignore = async (bot, msg) => {
     return remove_message(bot, msg);
 };
 
-const action_send_message = async (bot, text, options, msg, delete_calls) => {
+const action_send_message = async (bot, text, options, msg, { delete_calls }) => {
     if (delete_calls) {
         await remove_message(bot, msg);
         return send_message(bot, text, options, msg);
@@ -93,7 +94,7 @@ const action_send_message = async (bot, text, options, msg, delete_calls) => {
  * METHODS special actions
  ****************************************************************/
 
-const action_on_pin_one_language = async (bot, [ lang_arg ], msg, { keyword, lang }, { debug }) => {
+const action_on_pin_one_language = async (bot, [ lang_arg ], msg, { keyword, lang }, options) => {
     lang = lang || lang_arg || DEFAULT_LANGUAGE;
     // post menu:
     const responseText = get_translation(lang, keyword);
@@ -104,7 +105,7 @@ const action_on_pin_one_language = async (bot, [ lang_arg ], msg, { keyword, lan
     return pin_message(bot, reply);
 }
 
-const action_on_pin_all_languages = async (bot, msg, { keyword }, { debug }) => {
+const action_on_pin_all_languages = async (bot, msg, { keyword }, options) => {
     const n = SUPPORTED_LANGUAGES.length;
     let index = 0;
     for (const lang of SUPPORTED_LANGUAGES) {
@@ -127,31 +128,31 @@ const action_on_pin_all_languages = async (bot, msg, { keyword }, { debug }) => 
     return action_ignore();
 }
 
-const action_on_hello = async (bot, user, [ lang_arg ], msg, reply_to_msg, { keyword, lang }, { debug, delete_calls }) => {
+const action_on_hello = async (bot, user, [ lang_arg ], msg, reply_to_msg, { keyword, lang }, options) => {
     const username = user.user.first_name;
     const lang_caller = msg.from.language_code;
     lang = lang || lang_arg || lang_caller;
     // post text:
     const responseText = sprintf(get_translation(lang, keyword), username);
     const options = get_message_options_basic(reply_to_msg);
-    return action_send_message(bot, responseText, options, msg, delete_calls);
+    return action_send_message(bot, responseText, options, msg, options);
 }
 
-const action_on_help = async (bot, [ lang_arg ], msg, reply_to_msg, { keyword, lang }, { debug, delete_calls }) => {
+const action_on_help = async (bot, [ lang_arg ], msg, reply_to_msg, { keyword, lang }, options) => {
     lang = lang || lang_arg || DEFAULT_LANGUAGE;
     // post menu:
     const responseText = get_translation(lang, keyword);
     const options = get_main_menu_inline(lang, reply_to_msg);
-    return action_send_message(bot, responseText, options, msg, delete_calls);
+    return action_send_message(bot, responseText, options, msg, options);
 };
 
-const action_on_redirect = async (bot, [ lang_arg ], msg, reply_to_msg, { redirect }, { keyword, lang }, { debug, delete_calls }) => {
+const action_on_redirect = async (bot, [ lang_arg ], msg, reply_to_msg, { redirect }, { keyword, lang }, options) => {
     lang = lang || lang_arg || DEFAULT_LANGUAGE;
     // post text with link:
     const message = get_translation(lang, keyword);
     const responseText = `${message}: ${redirect}`;
     const options = get_message_options_basic(reply_to_msg);
-    return action_send_message(bot, responseText, options, msg, delete_calls);
+    return action_send_message(bot, responseText, options, msg, options);
 };
 
 /****************************************************************
--- src/parts/listeners.js
+++ src/parts/listeners.js
@@ -26,7 +26,7 @@ const listener_on_callback_query = async () => {
     console.warn('Not yet implemented');
 };
 
-const listener_on_text = async (bot, ctx, t, { debug, delete_calls, message_expiry }) => {
+const listener_on_text = async (bot, ctx, t, options) => {
     // do nothing if bot:
     if (user_in_context_is_bot(ctx)) return action_ignore();
     // otherwise ...
@@ -34,25 +34,26 @@ const listener_on_text = async (bot, ctx, t, { debug, delete_calls, message_expi
     const text = (msg.text || '').trim();
     if (is_valid_communication_pre(text)) {
         // if too old, delete and return:
+        const { message_expiry } = options;
         if (message_too_old(msg, t, message_expiry)) {
             return action_delete_and_ignore(bot, msg);
         }
         const botname = ctx.botInfo.username;
         const {commands, arguments} = filter_commands_by_command_pre(text, botname);
         if (commands.length > 0) {
-            return universal_action(bot, ctx, commands[0], arguments, { debug, delete_calls });
+            return universal_action(bot, ctx, commands[0], arguments, options);
         }
         // if invalid command, delete and return:
         return action_delete_and_ignore(bot, msg);
     } else if (text.startsWith(`/`)) {
-        return listener_on_message(bot, ctx, t, { debug, message_expiry });
+        return listener_on_message(bot, ctx, t, options);
     }
     // do nothing if not potentially a command:
     return action_ignore(bot, msg);
 }
 
 // handles inline user query:
-const listener_on_message = async (bot, ctx, t, { debug, delete_calls, message_expiry }) => {
+const listener_on_message = async (bot, ctx, t, options) => {
     // do nothing if bot:
     if (user_in_context_is_bot(ctx)) return action_ignore();
     // otherwise ...
@@ -60,13 +61,14 @@ const listener_on_message = async (bot, ctx, t, { debug, delete_calls, message_e
     const text = (msg.text || '').trim();
     if (is_valid_communication_post(text)) {
         // if too old, delete and return
+        const { message_expiry }  = options;
         if (message_too_old(msg, t, message_expiry)) {
             return action_delete_and_ignore(bot, msg);
         }
         const botname = ctx.botInfo.username;
         const {commands, arguments} = filter_commands_by_command_post(text, botname);
         if (commands.length > 0) {
-            return universal_action(bot, ctx, commands[0], arguments, { debug, delete_calls });
+            return universal_action(bot, ctx, commands[0], arguments, options);
         }
         // if invalid command, delete and return:
         return action_delete_and_ignore(bot, msg);
