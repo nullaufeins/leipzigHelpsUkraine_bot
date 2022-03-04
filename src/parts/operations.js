d73c8431c0dd2f14e3c80dca7607859eca73ff1e
RLogik <r.bitlogik@gmail.com>
2022-03-04 12:46:22 +0100

    staging > master: src - added operations for overwriting message

--- src/parts/operations.js
+++ src/parts/operations.js
@@ -14,10 +14,23 @@ const message_too_old = (msg, t, expiry) => {
     return (typeof date === 'number') ? (date * 1000 + expiry < t) : true;
 };
 
-const remove_message = async (bot, meta) => {
-    const chatId = meta.chat.id;
-    const message_id = meta.message_id;
-    bot.deleteMessage(chatId, message_id);
+const send_message = (bot, text, options, msg) => {
+    const chatId = msg.chat.id;
+    return bot.telegram.sendMessage(chatId, text, options);
+};
+
+const send_message_as_overwrite = (bot, text, options, msg) => {
+    const chatId = msg.chat.id;
+    const { message_id } = msg;
+    const { reply_markup } = options;
+    const { parse_mode } = reply_markup;
+    return bot.telegram.editMessageText(chatId, message_id, undefined, text, parse_mode, undefined, undefined, reply_markup);
+};
+
+const remove_message = async (bot, msg) => {
+    const chatId = msg.chat.id;
+    const { message_id } = msg;
+    bot.telegram.deleteMessage(chatId, message_id);
 };
 
 const delay_remove_reply = async (bot, timeout, reply) => {
@@ -36,6 +49,8 @@ const delay_remove_reply = async (bot, timeout, reply) => {
 
 module.exports = {
     message_too_old,
+    send_message,
+    send_message_as_overwrite,
     remove_message,
     delay_remove_reply,
 };
