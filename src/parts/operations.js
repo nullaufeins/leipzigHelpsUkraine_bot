a73f543a5b63f45bd91c35a11dd53a08154f3464
Kristin Fritsch <ich@kristin-fritsch.de>
2022-03-06 13:30:49 +0100

    More logging to debug lang feature

--- src/parts/operations.js
+++ src/parts/operations.js
@@ -11,6 +11,9 @@
 const get_language_sender_in_message = (msg) => {
     try {
         console.log(`Language from message: ${msg.from.language_code}`)
+        if (!msg.from.language_code) {
+            console.log(`From message: ${msg.from}`)
+        }
         return msg.from.language_code;
     } catch(_) {
         return undefined;
