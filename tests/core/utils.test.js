ed6c3749c6b0a79c2f49869ad0ceab3d3de42e5c
RLogik <r.bitlogik@gmail.com>
2022-03-08 17:54:48 +0100

    bug-catch-errors > staging: added unit test

--- tests/core/utils.test.js
+++ tests/core/utils.test.js
@@ -3,11 +3,14 @@
  ****************************************************************/
 
 const assert = require('assert');
+const expect = require('expect');
 
 const {
+    CombineErrors,
     split_non_empty_parts,
     take_one,
     yaml_to_js_dictionary,
+    try_action1_silently_then_perform_action2,
 } = require('../../src/core/utils.js');
 
 /****************************************************************
@@ -85,5 +88,62 @@ describe('Convert dictionary keys', () => {
                 'time_parameters': { max_delay: 78 }
             }
         );
+    });
+});
+
+describe('Perform actions1 silently then action2 and combine errors', () => {
+    it('should carry out both actions and not return errors', async () => {
+        let action1_done = false;
+        let action2_done = false;
+        let action1 = () => {
+            action1_done = true;
+            return Promise.resolve('value1');
+        };
+        let action2 = () => {
+            action2_done = true;
+            return Promise.resolve('value2');
+        };
+        let value = await try_action1_silently_then_perform_action2(action1, action2);
+        expect(value).toEqual('value2')
+        expect(action1_done).toEqual(true);
+        expect(action2_done).toEqual(true);
+    });
+
+    it('should ignore 1st error, carry out action2 and throw error1', async () => {
+        let action1_done = false;
+        let action2_done = false;
+        let action1 = () => {
+            action1_done = true;
+            return Promise.reject('Something went wrong!');
+        };
+        let action2 = () => {
+            action2_done = true;
+            return Promise.resolve('value2');
+        };
+        let err = await try_action1_silently_then_perform_action2(action1, action2)
+            .then((_) => {})
+            .catch((_) => _);
+        expect(err).toEqual('Something went wrong!')
+        expect(action1_done).toEqual(true);
+        expect(action2_done).toEqual(true);
+    });
+
+    it('should ignore 1st error, carry out action2 and throw both errors combined', async () => {
+        let action1_done = false;
+        let action2_done = false;
+        let action1 = () => {
+            action1_done = true;
+            return Promise.reject('Something went wrong!');
+        };
+        let action2 = () => {
+            action2_done = true;
+            return Promise.reject('Something else went wrong!');
+        };
+        let err = await try_action1_silently_then_perform_action2(action1, action2)
+            .then((_) => {})
+            .catch((_) => _);
+        expect(err).toEqual(CombineErrors('Something went wrong!', 'Something else went wrong!'));
+        expect(action1_done).toEqual(true);
+        expect(action2_done).toEqual(true);
     });
 });
